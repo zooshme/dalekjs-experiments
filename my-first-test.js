15180e616070f295d547fd2ab55dd24addbb36c4
zooshme <github@zoosh.me>
2014-09-26 18:25:03 +0100

    added screenshot

--- my-first-test.js
+++ my-first-test.js
@@ -1,3 +1,5 @@
+var path = require('path')
+
 module.exports = {
 	'Page title is correct': function(test) {
 		test
@@ -5,6 +7,7 @@ module.exports = {
 			.assert.title().is('Hello World', 'It has the correct title')
 			.wait(1000)
 			.assert.text('h1').is('muchos gracias!', 'The correct text')
+			.screenshot(path.join(__dirname, 'screenshots', 'whatever.jpg'))
 			.done();
 	}
 };
